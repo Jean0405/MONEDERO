b350f6847154ebddbf8978804768031f660b847c
Jean0405 <jeanpierrea0405@gmail.com>
2023-05-14 11:17:50 -0500

    fix: :bug: fix delete item mov

--- js/main.js
+++ js/main.js
@@ -1,113 +1,146 @@
-let listaMovimientos = []
-let totalDisponible = 0
-let ingresos = 0
-let egresos = 0
+let listaMovimientos = [];
+let totalDisponible = 0;
+let ingresos = 0;
+let egresos = 0;
 
 class Calculos {
-    constructor(movimiento, descripcion, valor) {
-        this.movimiento = movimiento;
-        this.descripcion = descripcion;
-        this.valor = valor;
-    }
-    disponible(lista, totalDisponible, ingresos, egresos) {
-        let mov = new Calculos();
-        let ui = new UI();
-        let valor = 0;
-
-        lista.forEach(item => {
-            valor = parseInt(item.valor);
-            switch (item.movimiento) {
-                case "ingresos":
-                    ingresos += valor;
-                    break;
-                case "egresos":
-                    egresos += valor;
-            }
-            totalDisponible = ingresos - egresos;
-        });
-        mov.porcentajes(egresos,ingresos);
-        ui.renderDisponible(totalDisponible,ingresos,egresos);
-        ui.renderDisponible(totalDisponible,ingresos,egresos);
-
-        let porcIngreso = Math.floor((valor/ingresos)*100) ;
-        let porcEgreso = Math.floor((valor/egresos)*100);
-
-        return {porcIngreso,porcEgreso};
-    }
-    porcentajes(egresos,ingresos){
-        let porcentajeEgresos;
-        let ui = new UI();
-
-        if (egresos!=0) {
-            porcentajeEgresos = Math.floor((egresos/ingresos)*100);
-            ui.renderPorcentajes(porcentajeEgresos)
-        }
-    }
-
-}
-
-
-class UI {
-    renderIngresos(item,porcIngreso) {
-        const ingresosContainer = document.querySelector('.ingresos');
-        const movIngresos = document.createElement('div');
-        movIngresos.setAttribute('class', 'ingresos-detalle col-12 d-flex justify-content-between py-2 my-1 rounded');
-            movIngresos.innerHTML = `
-            <span>${item.descripcion}</span>
-            <span class="valor-movimiento mx-2">+$${item.valor}</span>
-            <span class="porcentajeIn">${porcIngreso}%</span>
-        `;
-        ingresosContainer.appendChild(movIngresos);
-    }
-
-    renderEgresos(item,porcEgreso){
-
-        const egresosContainer = document.querySelector('.egresos');
-        const movEgresos = document.createElement('div');
-        movEgresos.setAttribute('class', 'egresos-detalle col-12 d-flex justify-content-between py-2 my-1 rounded');
-            movEgresos.innerHTML = `
-            <span>${item.descripcion}</span>
-            <span class="valor-movimiento mx-2">-$${item.valor}</span>
-            <span class="porcentajeEg">${porcEgreso}%</span>
-        `;
-            egresosContainer.appendChild(movEgresos);
-    }
-    renderDisponible(totalDisponible,ingresos,egresos){
-        document.querySelector('.disponible').innerHTML=`$${totalDisponible}`;
-        document.querySelector('.ingresos-total').innerHTML=`$${ingresos}`;
-        document.querySelector('.egresos-total').innerHTML=`$${egresos}`;
-    }
-
-    renderPorcentajes(porcentajeEgresos){
-        document.querySelector('.porcentaje-egresos').innerHTML =`${porcentajeEgresos}%`;
-    }
-    formReset() {
-        form.reset();
-    }
-}
-
-
-const form = document.querySelector('#formulario');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let data = Object.fromEntries(new FormData(e.target))
-    const { movimiento, descripcion, valor } = data; /*Desestructurando los valores*/
-
-
-    let mov = new Calculos(movimiento, descripcion, valor);
-    listaMovimientos.push(mov)
-    let porc = mov.disponible(listaMovimientos, totalDisponible, ingresos, egresos);
-    
-    const {porcIngreso, porcEgreso} = porc;
-    console.log(porcIngreso,porcEgreso)
+  constructor(movimiento, descripcion, valor) {
+    this.movimiento = movimiento;
+    this.descripcion = descripcion;
+    this.valor = valor;
+  }
+  disponible(lista, totalDisponible, ingresos, egresos) {
+    let valor = 0;
+    let mov = new Calculos();
     let ui = new UI();
 
+    lista.forEach((item) => {
+      valor = parseInt(item.valor);
+      switch (item.movimiento) {
+        case "ingresos":
+          ingresos += valor;
+          break;
+        case "egresos":
+          egresos += valor;
+      }
+      totalDisponible = ingresos - egresos;
+    });
 
-        if (mov.movimiento === "ingresos") {
-            ui.renderIngresos(mov,porcIngreso);
-        }else if(mov.movimiento === "egresos"){
-            ui.renderEgresos(mov,porcEgreso);
-        }
-    ui.formReset();
-})
+    mov.porcentajes(egresos, ingresos);
+    ui.renderDisponible(totalDisponible, ingresos, egresos);
+  }
+  porcentajes(egresos, ingresos) {
+    let porcentajeEgresos;
+    let ui = new UI();
+
+    if (egresos != 0) {
+      porcentajeEgresos = Math.floor((egresos / ingresos) * 100);
+      ui.renderPorcentajes(porcentajeEgresos);
+    }
+  }
+}
+
+class UI {
+  renderIngresos(item) {
+    const movIngresos = document.createElement("div");
+    movIngresos.setAttribute(
+      "class",
+      "ingresos-detalle detalle col-12 d-flex justify-content-around align-items-center py-2 my-1 rounded"
+    );
+    movIngresos.innerHTML = `
+            <span class="descripcion">${item.descripcion}</span>
+            <span class="valor-movimiento mx-2">+$${item.valor}</span>
+            <button class="btn borrar" id="borrarIn">X</button>
+        `;
+    ingresosContainer.appendChild(movIngresos);
+
+    const listaBtnBorrar = document.querySelectorAll("#borrarIn");
+    listaBtnBorrar.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const button = e.target;
+        borrarItemsMovimientos(button);
+      });
+    });
+  }
+
+  renderEgresos(item) {
+    const movEgresos = document.createElement("div");
+    movEgresos.setAttribute(
+      "class",
+      "egresos-detalle detalle col-12 d-flex justify-content-around align-items-center py-2 my-1 rounded"
+    );
+    movEgresos.innerHTML = `
+            <span class="descripcion">${item.descripcion}</span>
+            <span class="valor-movimiento mx-2">-$${item.valor}</span>
+            <button class="btn borrar" id="borrarEg">X</button>
+        `;
+    egresosContainer.appendChild(movEgresos);
+
+    const listaBtnBorrar = document.querySelectorAll("#borrarEg");
+    listaBtnBorrar.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const button = e.target;
+        borrarItemsMovimientos(button);
+      });
+    });
+  }
+  renderDisponible(totalDisponible, ingresos, egresos) {
+    document.querySelector(".disponible").innerHTML = `$${totalDisponible}`;
+    document.querySelector(".ingresos-total").innerHTML = `$${ingresos}`;
+    document.querySelector(".egresos-total").innerHTML = `$${egresos}`;
+  }
+
+  renderPorcentajes(porcentajeEgresos) {
+    document.querySelector(
+      ".porcentaje-egresos"
+    ).innerHTML = `${porcentajeEgresos}%`;
+  }
+  formReset() {
+    form.reset();
+  }
+}
+
+function borrarItemsMovimientos(button) {
+  const parentButton = button.closest(".detalle");
+  const nameItem = parentButton.querySelector(".descripcion").textContent;
+
+  listaMovimientos.forEach((item) => {
+    if (item.descripcion === nameItem) {
+      const index = listaMovimientos.indexOf(item);
+      listaMovimientos.splice(index, 1);
+      parentButton.remove();
+    }
+  });
+  let calculos = new Calculos();
+  calculos.disponible(listaMovimientos, totalDisponible, ingresos, egresos);
+}
+
+const form = document.querySelector("#formulario");
+const ingresosContainer = document.querySelector(".ingresos");
+const egresosContainer = document.querySelector(".egresos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let data = Object.fromEntries(new FormData(e.target));
+  const { movimiento, descripcion, valor } =
+    data; /*Desestructurando los valores*/
+
+  let mov = new Calculos(movimiento, descripcion, valor);
+  let ui = new UI();
+  listaMovimientos.push(mov);
+  mov.disponible(listaMovimientos, totalDisponible, ingresos, egresos);
+
+  ingresosContainer.innerHTML = "";
+  egresosContainer.innerHTML = "";
+
+  listaMovimientos.forEach((item) => {
+    if (item.movimiento === "ingresos") {
+      ui.renderIngresos(item);
+    } else if (item.movimiento === "egresos") {
+      ui.renderEgresos(item);
+    }
+  });
+
+  ui.formReset();
+});
